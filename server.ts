7792aa21fbb91d0a4016deffc9fb680bf8908f17
Paulo Vitor Freitas da Costa <107567673+pegadalg@users.noreply.github.com>
2023-09-04 20:27:20 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -7,4 +7,15 @@ app.listen({
 }).then(() => {
     console.log('HTTP server running on http://localhost3333')
 })
-
+app.get('/students',async () => {
+    const students =  await prisma.students.findMany()
+    return students
+})
+app.get('/teachers',async () => {
+    const teachers = await prisma.teachers.findMany()
+    return teachers
+})
+app.get('/subjects',async() => {
+    const subjects = await prisma.subjects.findMany()
+    return subjects
+})
